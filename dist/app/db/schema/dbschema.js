99e817d701f71ecf0199cc404c40aeec5adbd4b7
Edgard Rojas <edgard1394@gmail.com>
2022-03-07 11:57:58 -0500

    gitignore

--- dist/app/db/schema/dbschema.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=dbschema.js.map
--- dist/app/routes/api.campaign.routes.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-Object.defineProperty(exports, "__esModule", { value: true });
-const controller = require("../controllers/campaign.controller");
-module.exports = function (app) {
-    app.route('/campaign').post(controller.createCampaign);
-    app.route('/campaigns').get(controller.listCampaign);
-    app.route('/campaign/play/:id').get(controller.playCampaign);
-};
-//# sourceMappingURL=api.campaign.routes.js.map
