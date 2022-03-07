2047e68ef34be731a68d1af26b336c43e7b095f5
Angel Villalba <59578310+angelvillalba@users.noreply.github.com>
2022-03-07 11:40:54 -0500

    up

--- src/app/utils/template.utils.ts
+++ src/app/utils/template.utils.ts
@@ -1,23 +1,29 @@
 const nunjucks = require('nunjucks');
 
 nunjucks.configure({
-    autoescape: true,
-    tags: {
-        variableStart: '||',
-        variableEnd: '||'
-    }
+	autoescape: true,
+	tags: {
+		variableStart: '||',
+		variableEnd: '||'
+	}
 });
 
 export const makeHtml = function (templateData, datos) {
-    let objVariables = {};
-    let variables = JSON.parse(templateData.customVariables.replace(/\|/g, ''));
-    // console.log('Datos para plantilla:', datos);
-    // console.log('Variables: ', variables);
-    for (let v of variables) {
-        // console.log(v, datos[v]);
-        // eslint-disable-next-line no-prototype-builtins
-        objVariables[v] = datos.hasOwnProperty(v) ? datos[v] : '';
-    }
-    // console.log('Variables llenas: ', objVariables);
-    return nunjucks.renderString(templateData.htmlTemplate, objVariables);
+	try {
+		let objVariables = {};
+		let variables = JSON.parse(templateData.customVariables.replace(/\|/g, ''));
+		console.log('Datos para plantilla:', datos);
+		console.log('Variables: ', variables);
+		for (let v of variables) {
+			console.log(v, datos[v]);
+			// eslint-disable-next-line no-prototype-builtins
+			objVariables[v] = datos.hasOwnProperty(v) ? datos[v] : '';
+		}
+		console.log('Variables llenas: ', objVariables);
+		return nunjucks.renderString(templateData.htmlTemplate, objVariables);
+	} catch (error) {
+		console.log(error);
+		console.log('Error al generar la template: ', error.message);
+		return 'null';
+	}
 };
