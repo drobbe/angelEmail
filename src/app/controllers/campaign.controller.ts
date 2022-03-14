c7745ea723422a51292e498eaf65e5f7f9b7b1fe
Edgard Rojas <edgard1394@gmail.com>
2022-03-14 11:12:20 -0500

    UPPERCASE

--- src/app/controllers/campaign.controller.ts
+++ src/app/controllers/campaign.controller.ts
@@ -48,12 +48,19 @@ export const createCampaign = async (req: any, res: Response) => {
 
     dataExcel = dataExcel.map((row) => {
         for (let k in row) {
-            k = k.toLocaleUpperCase();
             if (
                 k.replace(/\s/g, '') !== k &&
                 Object.prototype.hasOwnProperty.call(row, k)
             ) {
                 row[k.replace(/\s/g, '-')] = row[k];
+                delete row[k];
+            }
+        }
+
+        for (let k in row) {
+            let upper = k.toUpperCase();
+            if (upper !== k) {
+                row[upper] = row[k];
                 delete row[k];
             }
         }
